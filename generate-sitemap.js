431c52a2a8240afcb4919088c1507ef062fc64f5
Cong Wang <owen_wangcong@hotmail.com>
2024-11-02 10:07:46 -0400

    Added site map generate code and include it in build process

---
+++ generate-sitemap.js
@@ -0,0 +1,115 @@
+const fs = require('fs');
+const path = require('path');
+const { format } = require('date-fns');
+
+const generateSitemapIndex = () => {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://qldazangjing.com/sitemap-main.xml</loc>
+  </sitemap>
+  <sitemap>
+    <loc>https://qldazangjing.com/sitemap-juans.xml</loc>
+  </sitemap>
+  <sitemap>
+    <loc>https://qldazangjing.com/sitemap-books.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+};
+
+const currentDate = format(new Date(), "yyyy-MM-dd");
+
+const generateMainSitemap = (data) => {
+  let urls = `
+    <url>
+        <loc>https://qldazangjing.com/</loc>
+        <lastmod>${currentDate}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+  
+    <url>
+        <loc>https://qldazangjing.com/search</loc>
+        <lastmod>${currentDate}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+
+    <url>
+        <loc>https://qldazangjing.com/dict</loc>
+        <lastmod>${currentDate}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+
+    <url>
+        <loc>https://qldazangjing.com/mystudy</loc>
+        <lastmod>${currentDate}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+  `;
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+    </urlset>`;
+};
+
+const generateJuansSitemap = (data) => {
+  let urls = '';
+
+  const firstLevelChildren = Object.keys(data);
+  firstLevelChildren.forEach((key) => {
+    urls += `
+        <url>
+          <loc>https://qldazangjing.com/juans/${key.replace('.htm', '').replace('ml', '')}</loc>
+          <lastmod>${currentDate}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.8</priority>
+        </url>`;
+  });
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls}
+</urlset>`;
+};
+
+const generateBooksSitemap = (data) => {
+  let urls = '';
+  for (const [fileName, content] of Object.entries(data)) {
+    content.bus.forEach((item) => {
+        urls += `
+        <url>
+          <loc>https://qldazangjing.com/books/${item.id}</loc>
+          <lastmod>${currentDate}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.8</priority>
+        </url>`;
+    });
+  }
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls}
+</urlset>`;
+};
+
+const generateStaticSitemaps = () => {
+  const filePath = path.join(process.cwd(), 'public', 'data', 'mls.json');
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const data = JSON.parse(fileContents);
+
+  const sitemapIndex = generateSitemapIndex();
+  const mainSitemap = generateMainSitemap(data);
+  const juansSitemap = generateJuansSitemap(data);
+  const booksSitemap = generateBooksSitemap(data);
+
+  fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap.xml'), sitemapIndex, 'utf8');
+  fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap-main.xml'), mainSitemap, 'utf8');
+  fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap-juans.xml'), juansSitemap, 'utf8');
+  fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap-books.xml'), booksSitemap, 'utf8');
+};
+
+generateStaticSitemaps(); 
