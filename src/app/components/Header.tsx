561f1a4094f667024bc3ffad23f89ed8b66148d2
Cong Wang <owen_wangcong@hotmail.com>
2024-11-18 17:44:56 -0500

    updated data-nosnippet attribute

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -186,12 +186,12 @@ const Header: React.FC = () => {
                 aria-label={isHeaderVisible ? "Hide Header" : "Show Header"}
               >
                 {isHeaderVisible ? 
-                  <div data-nosnippet className="w-5 h-5 flex items-center justify-center">隐</div> : 
-                  <div data-nosnippet className="w-5 h-5 flex items-center justify-center">显</div>
+                  <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">隐</div> : 
+                  <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">显</div>
                 }
               </button>
             </TooltipTrigger>
-            <TooltipContent>
+            <TooltipContent>  
               <Text>{isHeaderVisible ? "隐藏控制界面" : "显示控制界面"}</Text>
             </TooltipContent>
           </Tooltip>
@@ -206,7 +206,7 @@ const Header: React.FC = () => {
                         className="p-2 bg-card rounded-full shadow-md focus:outline-none hover:bg-primary-hover hover:text-primary-foreground-hover"
                         aria-label="Select Theme"
                       >
-                        <div data-nosnippet className="w-5 h-5 flex items-center justify-center">色</div>
+                        <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">色</div>
                       </button>
                     </DropdownMenu.Trigger>
                   </TooltipTrigger>
@@ -247,9 +247,9 @@ const Header: React.FC = () => {
                     aria-label={isSimplified ? "Switch to Traditional Chinese" : "Switch to Simplified Chinese"}
                   >
                     {isSimplified ? (
-                      <div data-nosnippet className="w-5 h-5 flex items-center justify-center">繁</div>
+                      <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">繁</div>
                     ) : (
-                      <div data-nosnippet className="w-5 h-5 flex items-center justify-center">简</div>
+                      <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">简</div>
                     )}
                   </button>
                 </TooltipTrigger>
@@ -267,7 +267,7 @@ const Header: React.FC = () => {
                         aria-label="Select Font and Width"
                       >
                         <div className="w-5 h-5 flex items-center justify-center">
-                          <div data-nosnippet className="w-5 h-5 flex items-center justify-center">字</div>
+                          <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">字</div>
                         </div>
                       </button>
                     </DialogTrigger>
@@ -387,7 +387,7 @@ const Header: React.FC = () => {
                           className="p-2 bg-card rounded-full shadow-md focus:outline-none hover:bg-primary-hover hover:text-primary-foreground-hover"
                           aria-label="Select Theme"
                         >
-                          <div data-nosnippet className="w-5 h-5 flex items-center justify-center">目</div>
+                          <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">目</div>
                         </button>
                       </DropdownMenu.Trigger>
                     </TooltipTrigger>
@@ -430,7 +430,7 @@ const Header: React.FC = () => {
                       }`}
                       aria-label="Add to Favorites"
                     >
-                      <div data-nosnippet className="w-5 h-5 flex items-center justify-center">藏</div>
+                      <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">藏</div>
                     </button>
                   </TooltipTrigger>
                   <TooltipContent>
@@ -448,7 +448,7 @@ const Header: React.FC = () => {
                       }`}
                       aria-label="Add to Bookmarks"
                     >
-                      <div data-nosnippet className="w-5 h-5 flex items-center justify-center">签</div>
+                      <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">签</div>
                     </button>
                   </TooltipTrigger>
                   <TooltipContent>
@@ -465,7 +465,7 @@ const Header: React.FC = () => {
                       className={`p-2 bg-card rounded-full shadow-md focus:outline-none hover:bg-primary-hover hover:text-primary-foreground-hover`}
                       aria-label="Download"
                     >
-                      <div data-nosnippet className="w-5 h-5 flex items-center justify-center">存</div>
+                      <div data-nosnippet="true" className="w-5 h-5 flex items-center justify-center">存</div>
                     </button>
                   </TooltipTrigger>
                   <TooltipContent>
